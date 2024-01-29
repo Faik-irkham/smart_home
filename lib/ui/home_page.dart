77205d439334f0a91e647ace57dcf1dfba37b7c1
faik-irkham <faikirkham@gmail.com>
2024-01-29 09:15:45 +0700

    add tab bar

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -14,7 +14,6 @@ class HomePage extends StatelessWidget {
           image: DecorationImage(
             image: AssetImage('assets/images/bg-home.jpg'),
             fit: BoxFit.cover,
-            
           ),
         ),
         child: ListView(
@@ -64,7 +63,11 @@ class HomePage extends StatelessWidget {
                   thisChild: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Icon(Icons.temple_hindu_sharp),
+                      Image.asset(
+                        'assets/icons/humidity.png',
+                        height: 28,
+                        color: whiteColor,
+                      ),
                       Text(
                         'Humidity',
                         style: TextStyle(
@@ -74,10 +77,10 @@ class HomePage extends StatelessWidget {
                         ),
                       ),
                       Text(
-                        '75%',
+                        '97%',
                         style: TextStyle(
                           color: whiteColor,
-                          fontSize: 14,
+                          fontSize: 16,
                           fontWeight: FontWeight.w500,
                         ),
                       ),
@@ -91,16 +94,27 @@ class HomePage extends StatelessWidget {
                   thisChild: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Icon(Icons.temple_hindu_sharp),
+                      Image.asset(
+                        'assets/icons/temperature.png',
+                        height: 28,
+                        color: whiteColor,
+                      ),
                       Text(
                         'Temperature',
                         style: TextStyle(
-                          color: blueColor,
+                          color: whiteColor,
                           fontSize: 10,
                           fontWeight: FontWeight.w500,
                         ),
                       ),
-                      Text('20 C'),
+                      Text(
+                        '20ºC',
+                        style: TextStyle(
+                          color: whiteColor,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
                     ],
                   ),
                 ),
@@ -111,20 +125,107 @@ class HomePage extends StatelessWidget {
                   thisChild: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Icon(Icons.temple_hindu_sharp),
+                      Image.asset(
+                        'assets/icons/wind.png',
+                        height: 28,
+                        color: whiteColor,
+                      ),
                       Text(
-                        'Temperature',
+                        'Wind',
                         style: TextStyle(
-                          color: blueColor,
+                          color: whiteColor,
                           fontSize: 10,
                           fontWeight: FontWeight.w500,
                         ),
                       ),
-                      Text('20 C'),
+                      Text(
+                        '23 m/s',
+                        style: TextStyle(
+                          color: whiteColor,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
                     ],
                   ),
                 ),
               ],
+            ),
+            const SizedBox(height: 25),
+            SizedBox(
+              height: 30,
+              child: DefaultTabController(
+                length: 4,
+                child: TabBar(
+                  indicator: const BoxDecoration(),
+                  dividerColor: Colors.transparent,
+                  isScrollable: true,
+                  tabAlignment: TabAlignment.start,
+                  labelColor: whiteColor,
+                  unselectedLabelColor: Colors.grey,
+                  labelStyle: const TextStyle(
+                    fontSize: 10,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  labelPadding: const EdgeInsets.symmetric(horizontal: 10),
+                  tabs: const [
+                    CustomGlass(
+                      thisWidth: 70,
+                      thisHeight: 30,
+                      thisRadius: 8,
+                      thisChild: Text(
+                        'Living Room',
+                      ),
+                    ),
+                    CustomGlass(
+                      thisWidth: 80,
+                      thisHeight: 30,
+                      thisRadius: 8,
+                      thisChild: Text(
+                        'Bedroom',
+                      ),
+                    ),
+                    CustomGlass(
+                      thisWidth: 80,
+                      thisHeight: 30,
+                      thisRadius: 8,
+                      thisChild: Text(
+                        'Bathroom',
+                      ),
+                    ),
+                    CustomGlass(
+                      thisWidth: 80,
+                      thisHeight: 30,
+                      thisRadius: 8,
+                      thisChild: Text(
+                        'Kitchen',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const Expanded(
+              child: TabBarView(
+                children: [
+                  // Tab 1 Content - Living Room
+                  Center(
+                    child: Text('Living Room'),
+                  ),
+                  // Tab 2 Content - Bedroom
+                  Center(
+                    child: Text('Bedroom'),
+                  ),
+                  // Tab 3 Content - Bathroom
+                  Center(
+                    child: Text('Bathroom'),
+                  ),
+                  // Tab 4 Content - Kitchen
+                  Center(
+                    child: Text('Kitchen'),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
